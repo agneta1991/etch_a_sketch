b9cfbbe5d4e20f32e937e0df4a75d26db0003c12
agneta1991 <pupienytea@gmail.com>
2023-03-08 15:26:27 +0200

    main css done

--- js.js
+++ js.js
@@ -1,1 +1,68 @@
-let container = document.getElementsByClassName('containerOne');
+const gridBox = document.querySelector('#gridBox');
+
+
+function myFunction() {
+
+    document.getElementById('myDropdown').classList.toggle('show');
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdownMenu");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function gridSizeSixteen() {
+    for (let i = 0; i < 256; i++) {
+        const content = document.createElement('div');
+        content.classList.add('gridSize');
+        gridBox.style.gridTemplateColumns = 'repeat(16, auto)';
+
+        gridBox.appendChild(content);
+    };
+
+}
+
+function gridSizeFourtyEight() {
+    for (let i = 0; i < 2304; i++) {
+        const content = document.createElement('div');
+        content.classList.add('gridSize');
+        gridBox.style.gridTemplateColumns = 'repeat(48, auto)';
+
+        gridBox.appendChild(content);
+    };
+
+}
+
+function gridSizeNinetySix() {
+    for (let i = 0; i < 9216; i++) {
+        const content = document.createElement('div');
+        content.classList.add('gridSize');
+        gridBox.style.gridTemplateColumns = 'repeat(96, auto)';
+
+        gridBox.appendChild(content);
+    };
+
+}
+
+function sixteen(){
+    gridBox.classList.remove('gridSize');
+    gridSizeSixteen();
+}
+
+function fourtyEight(){
+    gridBox.classList.remove('gridSize');
+    gridSizeFourtyEight();
+}
+
+function ninetySix(){
+    gridBox.classList.remove('gridSize');
+    gridSizeNinetySix();
+}
